3300deffde15c5f73910880663788694874938a8
lubuntu <MartinGiroux3@usherbrooke.ca>
2020-05-23 17:34:34 -0400

    tentative d,ajout temps restant

--- clock.js
+++ clock.js
@@ -15,9 +15,8 @@ export const toolbar = new class Temps{
 					label: `<span style="font-size: 150%" >00:00:00</span>`,
 					id:'heure',
 					hidden:false,
-					width:100
+					width:100,
 				},
-				{},
 				{
 					view:"bullet",
 					minRange:0,
@@ -28,21 +27,28 @@ export const toolbar = new class Temps{
 						{value:33, color:"red"},
 						{value:100, color:"green"},
 					]
+				},
+				{
+					view: "label",
+					label: `<span style="font-size: 150%" >09:59:59</span>`,
+					id:'temps_restant',
+					hidden:false,
+					width:100
 				}
 			]
 		}
 	}
 
-	updateClock(){ //Faudrait faire que ça tourne en boucle, pas trop sûr encore de comment faire ça
+	updateClock(){
 		var currentTime = new Date ();
 		var heure = currentTime.getHours();
 		var min = currentTime.getMinutes();
 		var sec = currentTime.getSeconds();
-		var total = heure*3600 + min*60 + sec;
-		var debutExam = 84820; //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
-		var finExam = 85000; //Idem
-		var tiersTemps = (finExam-debutExam)/3 + debutExam;
-
+		var debutExam = new Date(2020, 11, 15, 10, 15, 20); //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
+		var finExam = new Date(2020, 11, 15, 12, 0, 0); //Idem
+		var debutExamEnSec = 3600*debutExam.getHours() + 60*debutExam.getMinutes() + debutExam.getSeconds();
+		var finExamEnSec = 3600*finExam.getHours() + 60*finExam.getMinutes() + finExam.getSeconds();
+		var currentTimeEnSec = 3600*currentTime.getHours() + 60*currentTime.getMinutes() + currentTime.getSeconds();
 		min = (min < 10 ? "0" : "") + min;
 		sec = (sec < 10 ? "0" : "") + sec;
 
@@ -52,9 +58,20 @@ export const toolbar = new class Temps{
 
 		var stringHeure = heure + ":" + min + ":" + sec + " " + AMPM;
 
-		this[0].setValue(stringHeure);
+		this[0].define("label", "stringHeure");
+		this[0].refresh();
 
-		if(total <= finExam)
-			this[1].setValue((total-debutExam)*100/(finExam-debutExam));
+		if(currentTime <= finExam)
+			this[1].setValue((currentTimeEnSec-debutExamEnSec)*60/(finExamEnSec-debutExamEnSec));
+
+		var heure2 = finExam.getHours() - currentTime.getHours();
+		var minutes2 = finExam.getMinutes() - currentTime.getMinutes();
+		var secondes2 = finExam.getSeconds() - currentTime.getSeconds();
+		minutes2 = (min < 10 ? "0" : "") + min;
+		secondes2 = (sec < 10 ? "0" : "") + sec;
+
+		var stringHeure2 = heure2 + ":" + minutes2 + ":" + secondes2;
+
+		this[2].define("label", "stringHeure2");
 	}
 }
