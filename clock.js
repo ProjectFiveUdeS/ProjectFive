e04dcc10732dff8a76e95ba74848cfccc4dc2bcc
lubuntu <MartinGiroux3@usherbrooke.ca>
2020-05-23 14:09:10 -0400

    réglage de bugs

--- clock.js
+++ clock.js
@@ -1,69 +1,63 @@
-var tempsLabel = webix.ui({
-	view: "label",
-	label: "00:00:00",
-	inputWidth: 100,
-	align: "center"
-});
-
-var barreProgres = webix.ui({
-	view:"bullet",
-	minRange:0,
-	maxRange:100,
-	value:0,
-	flowTime:1000,
-	bands:[
-		{ value:100, color:"green" },
-		{ value:33, color:"red" },
-	],
-	label:"Examen", //Sera modifié via la DB
-	placeholder:"cours",
-	marker:70,
-	stroke:8,
-	scale:{
-		step:10,
-		template:"#value#%"
-	}
-});
-
-function updateClock(){
-	var currentTime = new Date ();
-	var heure = currentTime.getHours();
-	var min = currentTime.getMinutes();
-	var sec = currentTime.getSeconds();
-	var total = heure*3600 + min*60 + sec;
-	var debutExam = 84820; //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
-	var finExam = 85000; //Idem
-	var tiersTemps = (finExam-debutExam)/3 + debutExam;
-
-	min = (min < 10 ? "0" : "") + min;
-	sec = (sec < 10 ? "0" : "") + sec;
-
-	var AMPM = (heure < 12) ? "AM" : "PM";
-	heure = (heure > 12) ? heure - 12 : heure;
-	heure = (heure == 0) ? 12 : heure;
-
-	var stringHeure = heure + ":" + min + " " + AMPM;
-
-	tempsLabel.setValue(stringHeure);
-	barreProgres.setValue((total-debutExam)*100/(finExam-debutExam));
-}
-
-function progressBar(){
-	var currentTime = new Date ();
-	var heure = currentTime.getHours();
-	var min = currentTime.getMinutes();
-	var sec = currentTime.getSeconds();
-
-
-	if(total >= tiersTemps)
-	{
-		document.getElementById("Barre").style.backgroundColor = "green";
+export const toolbar = new class Temps{
+	getViewID(){
+		return "toolbar";
 	}
 
-	if(total <= finExam)
-	{
-		document.getElementById("Barre").style.width = (total - debutExam)*100/(finExam-debutExam) + "%";
+	get configuration() {
+		return {
+			view: "toolbar",
+			id: this.getViewID(),
+			paddingY:1,
+			hidden:false,
+			height:40, elements: [
+				{
+					view: "label",
+					label: `<span style="font-size: 150%" >00:00:00</span>`,
+					id:'heure',
+					hidden:false,
+				},
+				{},
+				{
+					view:"bullet",
+					minRange:0,
+					maxRange:100,
+					value:0,
+					flowTime:1000,
+					bands:[
+						{value:33, color:"red"},
+						{value:100, color:"green"},
+					],
+					scale:{
+						step:10,
+						template:"#value#%"
+					}
+				}
+			]
+		}
 	}
-	else
-		return;
+
+	updateClock(){ //Faudrait faire que ça tourne en boucle, pas trop sûr encore de comment faire ça
+		var currentTime = new Date ();
+		var heure = currentTime.getHours();
+		var min = currentTime.getMinutes();
+		var sec = currentTime.getSeconds();
+		var total = heure*3600 + min*60 + sec;
+		var debutExam = 84820; //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
+		var finExam = 85000; //Idem
+		var tiersTemps = (finExam-debutExam)/3 + debutExam;
+
+		min = (min < 10 ? "0" : "") + min;
+		sec = (sec < 10 ? "0" : "") + sec;
+
+		var AMPM = (heure < 12) ? "AM" : "PM";
+		heure = (heure > 12) ? heure - 12 : heure;
+		heure = (heure == 0) ? 12 : heure;
+
+		var stringHeure = heure + ":" + min + ":" + sec + " " + AMPM;
+
+		this[0].setValue(stringHeure);
+
+		if(total <= finExam)
+			this[1].setValue((total-debutExam)*100/(finExam-debutExam));
+	}
 }
