b1278830d2f70d54132c63863c88053a1d885ac6
lubuntu <MartinGiroux3@usherbrooke.ca>
2020-06-11 19:32:50 -0400

    HORLOGE FONCTIONNELLE (avec quelques bugs quand mm)

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,6 @@
 import {datatable} from "./datatable.js";
 import {layout} from "./layout.js";
+import {Clock} from "./clock.js"
 
 webix.attachEvent(
     'onAjaxError',
@@ -28,8 +29,9 @@ webix.attachEvent(
         webix.i18n.setLocale('fr-FR');   // set notation
         webix.ui(layout.configuration);  // établi les grandes lignes de l'interface usager
         webix.extend($$("main"), webix.ProgressBar);  //Met en place une barre d'information pour la sauvegarde
-        //const parameters = Object.fromEntries(new URLSearchParams(window.location.search)) // va chercher les parametres du URL
-        //await datatable.loadTrimestre(); //initialize les données à partir de service res
-        Clock.updateClock();
+        const parameters = Object.fromEntries(new URLSearchParams(window.location.search)) // va chercher les parametres du URL
+        await datatable.loadTrimestre(); //initialize les données à partir de service rest
+        let clock = new Clock();
+        setInterval(()=>clock.updateClock(),1000);
     }
 )();
--- src/main/webapp/clock.js
+++ src/main/webapp/clock.js
@@ -1,41 +1,46 @@
-new class Clock{
-    static updateClock();
+export class Clock{
 
-     updateClock = function(){
-     let currentTime = new Date ();
-     let heure = currentTime.getHours();
-     let min = currentTime.getMinutes();
-     let sec = currentTime.getSeconds();
-     const debutExam = new Date(2020, 11, 15, 10, 15, 20); //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
-     const finExam = new Date(2020, 11, 15, 12, 0, 0); //Idem
-     const debutExamEnSec = 3600*debutExam.getHours() + 60*debutExam.getMinutes() + debutExam.getSeconds();
-     const finExamEnSec = 3600*finExam.getHours() + 60*finExam.getMinutes() + finExam.getSeconds();
-     const currentTimeEnSec = 3600*currentTime.getHours() + 60*currentTime.getMinutes() + currentTime.getSeconds();
-     min = (min < 10 ? "0" : "") + min;
-     sec = (sec < 10 ? "0" : "") + sec;
+    updateClock(){
+        let currentTime = new Date ();
+        let heure = currentTime.getHours();
+        let min = currentTime.getMinutes();
+        let sec = currentTime.getSeconds();
+        const debutExam = new Date(2020, 6, 11, 18, 30, 20); //Valeur convertie en secondes, on récupérera cette valeur dans la DB.
+        const finExam = new Date(2020, 6, 11, 19, 20, 0); //Idem
+        const debutExamEnSec = 3600*debutExam.getHours() + 60*debutExam.getMinutes() + debutExam.getSeconds();
+        const finExamEnSec = 3600*finExam.getHours() + 60*finExam.getMinutes() + finExam.getSeconds();
+        const currentTimeEnSec = 3600*currentTime.getHours() + 60*currentTime.getMinutes() + currentTime.getSeconds();
+        min = (min < 10 ? "0" : "") + min;
+        sec = (sec < 10 ? "0" : "") + sec;
 
-     let AMPM = (heure < 12) ? "AM" : "PM";
-     heure = (heure > 12) ? heure - 12 : heure;
-     heure = (heure == 0) ? 12 : heure;
+        let AMPM = (heure < 12) ? "AM" : "PM";
+        heure = (heure > 12) ? heure - 12 : heure;
+        heure = (heure == 0) ? 12 : heure;
 
-     let stringHeure = heure + ":" + min + ":" + sec + " " + AMPM;
-     $$("heure_actuelle").setValue(stringHeure);
-     $$("heure_actuelle").refresh();
+        let stringHeure = heure + ":" + min + ":" + sec + " " + AMPM;
+        $$("heure_actuelle").setValue(stringHeure);
+        $$("heure_actuelle").refresh();
 
-     if(currentTime <= finExam)
-         $$("barre_temps_restant").setValue((currentTimeEnSec-debutExamEnSec)*60/(finExamEnSec-debutExamEnSec));
-     $$("barre_temps_restant").refresh();
+        if(currentTime <= finExam)
+            $$("barre_temps_restant").setValue((currentTimeEnSec-debutExamEnSec)*100/(finExamEnSec-debutExamEnSec));
+        $$("barre_temps_restant").refresh();
 
-     let heure2 = finExam.getHours() - currentTime.getHours();
-     let minutes2 = finExam.getMinutes() - currentTime.getMinutes();
-     let secondes2 = finExam.getSeconds() - currentTime.getSeconds();
-     minutes2 = (min < 10 ? "0" : "") + min;
-     secondes2 = (sec < 10 ? "0" : "") + sec;
+        let heure2 = finExam.getHours() - currentTime.getHours();
+        let minutes2 = finExam.getMinutes() - currentTime.getMinutes();
+        let secondes2 = finExam.getSeconds() - currentTime.getSeconds();
 
-     let stringHeure2 = heure2 + ":" + minutes2 + ":" + secondes2;
+        if(minutes2 < 0)
+            minutes2 += 59;
+        if(secondes2 < 0)
+            secondes2 += 60;
 
-     $$("temps_restant").setValue(stringHeure2);
-     $$("temps_restant").refresh();
+        heure2 = (heure2 < 10 ? "0" : "") + heure2;
+        minutes2 = (minutes2 < 10 ? "0" : "") + minutes2;
+        secondes2 = (secondes2 < 10 ? "0" : "") + secondes2;
+
+        let stringHeure2 = heure2 + ":" + minutes2 + ":" + secondes2;
+
+        $$("temps_restant").setValue(stringHeure2);
+        $$("temps_restant").refresh();
     }
 }
-export{Clock};
