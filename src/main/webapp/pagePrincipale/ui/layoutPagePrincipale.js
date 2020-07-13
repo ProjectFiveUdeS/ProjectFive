f5ac7e9c8bc5c450de60974c32ae8589b74561fb
Martin <girm1002@usherbrooke.ca>
2020-07-12 23:07:36 -0400

    ajout du bandeau en haut

--- src/main/webapp/pagePrincipale/ui/layoutPagePrincipale.js
+++ src/main/webapp/pagePrincipale/ui/layoutPagePrincipale.js
@@ -1,6 +1,7 @@
 import {bottom} from "./bottom.js";
 import {layoutEtage2} from "./layoutEtage2.js";
 import {layoutEtage1} from "./layoutEtage1.js";
+import {top} from "./top.js";
 
 export const layoutPagePrincipale = new class {
 
@@ -20,10 +21,9 @@ export const layoutPagePrincipale = new class {
             id: 'main',
             css: "my_style",
             rows: [
+                top.configuration,
                 layoutEtage1.configuration,
-                {},
                 layoutEtage2.configuration,
-                {},
                 bottom.configuration,
             ],
         }
--- src/main/webapp/pagePrincipale/ui/top.js
+++ src/main/webapp/pagePrincipale/ui/top.js
@@ -1,1 +1,21 @@
-//Département de génie électrique et informatique, blablabla
+export const top = new class Top  {
+
+
+    get id() {
+        return "top"
+    }
+
+    get configuration() {
+        return {
+            view: "toolbar",
+            id: this.id,
+            hidden: false,
+            elements: [{
+                view: "label",
+                height: 123,
+                label: '<img src="../images/gestion%20examen.PNG" width="100%">'
+            }
+            ]
+        }
+    }
+};
