0a4a0675d51e816fccd7a6bae0d536c95523e3dc
AmauryPG <amaury.pala@outlook.com>
2020-07-16 14:16:35 -0400

    les bandeaux sont de la bonne couleur

--- src/main/webapp/pageGestionSurveillant/layoutPrincipal.js
+++ src/main/webapp/pageGestionSurveillant/layoutPrincipal.js
@@ -19,12 +19,14 @@ export const layoutPrincipal = new class {
         return {
             view: 'form',
             id: 'main',
+            css: 'my_style',
             /*padding:{
                 top:0, bottom:0, left:100, right:0
             },*/
             paddingX:-5,
             paddingY:-5,
             multiselect:true,
+
             rows: [
                 bandeau.configuration,
                 layoutSurveillantUnis.configuration,
